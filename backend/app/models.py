8872f60f29144c9b2f2f30f2db3b2845bd3a41b4
Shanelle Tan <shanelletan.me@gmail.com>
2026-03-10 10:28:10 +1100

    update json data structure

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel
 from typing import List, Optional
 
-# This represents one row in your Excel (20 columns)
 class Event(BaseModel):
     date_start: str
     date_end: str
@@ -12,7 +11,6 @@ class Event(BaseModel):
     ten_year_trend: Optional[str] = None
     ten_year_percent_change: Optional[float] = None
 
-# This represents the final JSON file structure
 class CrimeDataExport(BaseModel):
     data_source: str
     data_type: str
