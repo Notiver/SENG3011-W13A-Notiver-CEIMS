7995b226e7e06f31cad4d73619431a0fd35fcf24
Pierre <z5482784@ad.unsw.edu.au>
2026-03-16 16:38:39 +1100

    changing the file infrastucutre based on lambda api gateway which is serverless

---
+++ backend/services/data-collection/app/services/article_manager.py
@@ -0,0 +1,25 @@
+import os
+from app.utils.fetch_urls import get_random_monthly_articles
+from app.utils.article_scraper import process_articles
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+URL_FILE_PATH = os.path.join(BASE_DIR, "..", "..", "guardian_crime_urls.txt")
+
+def execute_full_collection():
+    try:
+        # Get URLs + save them to the text file
+        get_random_monthly_articles(2021, 2025, filename=URL_FILE_PATH)
+        
+        # Read text file, scrape Newspaper4k, process
+        process_articles(file_path=URL_FILE_PATH)
+        return {"status": "success", "message": "Articles collected and processed successfully."}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
+
+def fetch_collection_status():
+    return {"status": "success", "message": "System is ready for collection."}
+
+
+  # USE BOTO!!, what im up to: need to download docker and put the data processing to docker, turn that to lambda,
+  #  connect API gateway for all of them.
+  # Fix the routing path and how the code communicates with eachother. 
