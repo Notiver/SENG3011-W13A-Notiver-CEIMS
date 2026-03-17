3c1162af3e41df6a31280a00de8fc09aa929ec37
Orla Haughton <orla.haughton@student.unsw.edu.au>
2026-03-17 22:57:09 +1100

    Added route prefixes

--- backend/services/data-retrieval/app/main.py
+++ backend/services/data-retrieval/app/main.py
@@ -3,5 +3,5 @@ from app.api.routes import router
 from mangum import Mangum
 
 app = FastAPI(title="Notiver Retrieval API")
-app.include_router(router)
+app.include_router(router, prefix="/data-retrieval")
 handler = Mangum(app)
