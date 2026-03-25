b914951aa597de8213ba57eef016a5960c584b38
Pierre <z5482784@ad.unsw.edu.au>
2026-03-25 23:03:20 +1100

    configured CORS in main to handle route specific permissions for wildcard server calls

--- backend/services/data-processing/app/main.py
+++ backend/services/data-processing/app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from app.api.routes import router
 from mangum import Mangum
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="Notiver NLP Processing API", root_path="/data-processing")
 app.include_router(router)
 handler = Mangum(app)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
