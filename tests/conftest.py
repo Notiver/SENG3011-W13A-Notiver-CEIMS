88fe786acd94407116e8e50b1545fa3654542675
Shanelle Tan <shanelletan.me@gmail.com>
2026-04-03 17:43:26 +1100

    fix comments

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,17 +14,9 @@ def wait_for_s3_object(s3, bucket, key):
     '''waits for uploads to s3'''
     waiter = s3.get_waiter('object_exists')
     try:
-        # Defaults to checking every 5 seconds, 20 times.
-        # You can customize these via Delay and MaxAttempts.
         waiter.wait(Bucket=bucket, Key=key, \
                     WaiterConfig={'Delay': 1, 'MaxAttempts': TIMEOUT})
         return True
     except Exception as e:
         print(f"Timed out or error: {e}")
         return False
-
-# def get_user_id_from_token(token):
-#     # Decode the payload without verifying the signature
-#     # (Safe for local test scripts where you already trust the source)
-#     decoded = jwt.decode(token, options={"verify_signature": False})
-#     return decoded.get("sub")
