dbf329a1bdee0a98288c2cd4c313fc3b9f9f213e
Zachary Lin <z5488105@ad.unsw.edu.au>
2026-04-03 20:50:30 +1100

    uncommenting required code

--- backend/services/data-retrieval/app/main.py
+++ backend/services/data-retrieval/app/main.py
@@ -10,7 +10,7 @@ from observability.middleware.rate_limiter import limiter
 from observability.middleware.logging_middleware import observability_middleware
 from aws_lambda_powertools import Metrics, Tracer
 from aws_xray_sdk.core import patch_all
-# from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 patch_all()
 tracer = Tracer(service="data-retrieval")
@@ -19,17 +19,17 @@ app = FastAPI(title="Notiver Retrieval API")
 
 # metrics.set_default_dimensions(service="data-retrieval")
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=[
-#       "https://main.d2exnodyaugt1a.amplifyapp.com",
-#       "https://staging.d2exnodyaugt1a.amplifyapp.com",
-#       "http://localhost:3000"
-#     ],
-#     allow_credentials=True,
-#     allow_methods=["*"], 
-#     allow_headers=["*"], 
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+      "https://main.d2exnodyaugt1a.amplifyapp.com",
+      "https://staging.d2exnodyaugt1a.amplifyapp.com",
+      "http://localhost:3000"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
 
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
