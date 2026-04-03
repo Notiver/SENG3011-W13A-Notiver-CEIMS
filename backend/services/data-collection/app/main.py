a654fa3cac979316e6b0e451a4e85211329cf526
Zachary Lin <z5488105@ad.unsw.edu.au>
2026-04-03 13:30:17 +1100

    linting

--- backend/services/data-collection/app/main.py
+++ backend/services/data-collection/app/main.py
@@ -38,10 +38,10 @@ app.middleware("http")(observability_middleware)
 
 app.include_router(router)
 stage = os.getenv("STAGE", "staging")
-_mangum_handler = Mangum(app, lifespan="off", api_gateway_base_path=f"/data-collection")
+_mangum_handler = Mangum(app, lifespan="off", api_gateway_base_path="/data-collection")
 
 @tracer.capture_lambda_handler
 def handler(event, context):
     stage = os.getenv("STAGE", "staging")
-    _mangum_handler = Mangum(app, lifespan="off", api_gateway_base_path=f"/data-collection")
+    _mangum_handler = Mangum(app, lifespan="off", api_gateway_base_path="/data-collection")
     return _mangum_handler(event, context)
